3921b747b6bf4358857fa23c6b9bde60c9e37f9d
nazrullayevkamoliddin <kamolxoja.n@gmail.com>
2023-08-18 19:07:13 +0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+  import Player from "./Player.svelte";
+
+  let redScore = 20;
+  let blueScore = 20;
+  $: blueWon = redScore <= 0;
+  $: redWon = blueScore <= 0;
+  $: gameOver = blueWon || redWon;
+</script>
+
+<main>
+  <h1>Magic The Gathering Game</h1>
+  <div id="controls-container">
+    <Player
+      {gameOver}
+      fontColor="#0000AA"
+      won={blueWon}
+      winningText="Blue Wins"
+      score={blueScore}
+    />
+    <Player
+      {gameOver}
+      fontColor="#AA0000"
+      won={redWon}
+      winningText="Red Wins"
+      score={redScore}
+    />
+  </div>
+  <button>Start Game</button>
+</main>
+
+<style>
+  main {
+    width: 50%;
+    text-align: center;
+    padding: 20px;
+    border: solid gray 1px;
+    margin: 0 auto;
+    background-color: wheat;
+    margin: 10vh auto;
+  }
+  #controls-container {
+    display: flex;
+  }
+  button {
+    display: block;
+    width: 100%;
+    margin-top: 20px;
+    border: solid salmon 1px;
+    background-color: sandybrown;
+    color: rgb(61, 56, 56);
+    font-size: 20px;
+    border-radius: 3px;
+  }
+</style>
---
+++ src/Player.svelte
@@ -0,0 +1,40 @@
+<script>
+  export let score;
+  export let winningText;
+  export let won;
+  export let fontColor;
+  export let gameOver;
+</script>
+
+<div style="color: {fontColor};" class="player">
+  <h2>{score}</h2>
+  <button disabled={gameOver} class="plus">+</button>
+  <button disabled={gameOver} class="minus">-</button>
+  {#if won}
+    <h2>{winningText}</h2>
+  {/if}
+</div>
+
+<style>
+  .player {
+    flex-grow: 1;
+  }
+  .plus {
+    background-color: seagreen;
+  }
+  .minus {
+    background-color: brown;
+  }
+  button[disabled] {
+    background-color: gray;
+    pointer-events: none;
+  }
+  button {
+    font-size: 20px;
+    border-radius: 3px;
+    width: 40px;
+    color: white;
+    font-family: monospace;
+    font-weight: bold;
+  }
+</style>
